f9e9bd082c697b76a20cba3c83b6a7cc943bf66c
kangjaspreet <jaspreetkang@ucla.edu>
2022-10-14 21:57:04 -0700

    Add bulleted list with brackets

---
+++ R/homepage_featuredStory.R
@@ -0,0 +1,53 @@
+featuredStory <- function(
+  myURL,
+  myImage,
+  myTitle,
+  myDescription
+) {
+  HTML(
+    paste0(
+      "<div class = 'featuredStory'>
+        <a href='", myURL, "'>
+          <span class='link-spanner'></span>
+        </a>
+        <img src='Featured Stories/", myImage, "'>
+        <h1>", myTitle, "</h1>
+        <p>", myDescription, "</p>
+      </div>"
+    )
+  )
+}
+
+
+feature_shaCM <- featuredStory(
+  myURL = "https://skylab.cdph.ca.gov/communityBurden/_w_ac3e8cdd/SOPH/2022/Full%20Report.html",
+  myImage = "SHA CM - Image.png",
+  myTitle = "State Health Assessment Core Module - 2022",
+  myDescription = "This annual State Health Assessment (SHA) Core Module provides a snapshot of the health status for the entire California population. The Module is based upon a set of standard inputs, produced using an automated system, to assess population health across a range of health conditions, demographic characteristics, and other factors (e.g., disparities and inequities). The Module is used to identify key findings that contribute to informing the State Health Improvement Plan."
+  
+)
+
+feature_xMDB <- featuredStory(
+  myURL = "https://skylab.cdph.ca.gov/communityBurden/_w_ac3e8cdd/xMDA/2020_Excess_Mortality.html",
+  myImage = "Excess Mortality Data Brief - Image.jpg",
+  myTitle = "2020/2021 Excess Mortality Data Brief",
+  myDescription = "This annual State Health Assessment (SHA) Core Module provides a snapshot of the health status for the entire California population. The Module is based upon a set of standard inputs, produced using an automated system, to assess population health across a range of health conditions, demographic characteristics, and other factors (e.g., disparities and inequities). The Module is used to identify key findings that contribute to informing the State Health Improvement Plan."
+  
+)
+
+feature_lghcChildhood <- featuredStory(
+  myURL = "https://letsgethealthy.ca.gov/the-story/",
+  myImage = "LGHC Childhood Wellbeing - Image.jfif",
+  myTitle = "Let's Get Healthy CA Story - Early Childhood Well-Being",
+  myDescription = "This annual State Health Assessment (SHA) Core Module provides a snapshot of the health status for the entire California population. The Module is based upon a set of standard inputs, produced using an automated system, to assess population health across a range of health conditions, demographic characteristics, and other factors (e.g., disparities and inequities). The Module is used to identify key findings that contribute to informing the State Health Improvement Plan."
+  
+)
+
+feature_scoda <- featuredStory(
+  myURL = "https://skylab.cdph.ca.gov/communityBurden/_w_ac3e8cdd/SOPH/2022/Full%20Report.html",
+  myImage = "SCODA - Image.png",
+  myTitle = "Social Determinant of Health-Cause of Death Analysis",
+  myDescription = "This annual State Health Assessment (SHA) Core Module provides a snapshot of the health status for the entire California population. The Module is based upon a set of standard inputs, produced using an automated system, to assess population health across a range of health conditions, demographic characteristics, and other factors (e.g., disparities and inequities). The Module is used to identify key findings that contribute to informing the State Health Improvement Plan."
+  
+)
+
--- R/tab_homepage.R
+++ R/tab_homepage.R
@@ -2,19 +2,71 @@ home_ui <- function(id) {
   ns <- NS(id)
   tagList(
     div(class = "homeMainContent",
-    # - Featured Stories ------------------------------------------------------------------
-      div(class = "featuredStoryRow",
-        fluidRow(
-          column(width = 3, feature_shaCM), 
-          column(width = 3, feature_xMDB), 
-          column(width = 3, feature_lghcChildhood), 
-          column(width = 3, feature_scoda)))
+        # - CCB Anatomy ----------------------------------------------------------------------
+        div(class = "anatomyRow", 
+            fluidRow(
+              column(width = 2, 
+                     radioGroupButtons(inputId = ns("menu_inputs"), 
+                                       label = h4("Dyanmic Menu"), 
+                                       choiceValues = list("mapsTab", "trendsTab", "rankByCauseTab", 
+                                                           "rankByGeographyTab", "ageRaceFocusTab", "deathHospEdTab", 
+                                                           "hospitalizationsTab", "disparitiesTab", "sdohTab", 
+                                                           "demographicsTab"), 
+                                       choiceNames = list("MAPS", "TRENDS", "RANK BY CAUSE", "RANK BY GEOGRAPHY", 
+                                                          "AGE RACE FOCUS", "DEATH HOSP ED", "HOSPITALIZATIONS", 
+                                                          "DISPARITIES", "SDOH", "DEMOGRAPHICS"), 
+                                       direction = "vertical", 
+                                       justified = TRUE, 
+                                       individual = TRUE, 
+                                       status = "anatomyMenu")),
+              column(width = 3, HTML("<div class = 'bracket-dividers'>
+                                        <ul>
+                                          <li>Geography
+                                            <ul class='secondLevel'>
+                                              <li><strong>State</strong></li>
+                                              <li>County</li>
+                                            </ul>
+                                          </li>
+                                          <li>Demographics
+                                            <ul class='secondLevel'>
+                                              <li>Sex</li>
+                                              <li>Age</li>
+                                              <li><strong>Race/Ethnicity</strong></li>
+                                              <li>Educational Attainment</li>
+                                            </ul>
+                                          </li>
+                                          <li>Causes of Death</li>
+                                          <li>Death measures
+                                            <ul class='secondLevel'>
+                                              <li>Numbers</li>
+                                              <li><strong>Adjusted Rates</strong></li>
+                                              <li>Years of Life Lost</li>
+                                            </ul>
+                                          </li>
+                                        </ul>
+                                      </div>")),
+              column(width = 7, plotOutput(outputId = ns("preview_plot")))
+            )),
+        br(), br(),
+        # - Featured Stories ------------------------------------------------------------------
+        div(class = "featuredStoryRow",
+            fluidRow(class = "test",
+                     column(width = 3, feature_shaCM), 
+                     column(width = 3, feature_xMDB), 
+                     column(width = 3, feature_lghcChildhood), 
+                     column(width = 3, feature_scoda)))
     )
   )
 }
 
 home_server <- function(id) {
   moduleServer(id, function(input, output, session) {
+    
+    output$preview_plot <- renderPlot({
+      mtcars %>% 
+        ggplot(aes(x = mpg, y = disp)) +
+        geom_point()
+    })
     
   })
 }
